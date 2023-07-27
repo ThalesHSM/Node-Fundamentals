22291e338819e1603e4e381ab6374734d414e33a
ThalesHSM <thsm271@gmail.com>
2023-07-26 23:29:27 -0300

    Removed unused code

--- streams/stream-http-server.js
+++ streams/stream-http-server.js
@@ -1,16 +1,6 @@
 import http from 'node:http'
 import { Transform } from 'node:stream'
 
-class InverseNumberStream extends Transform {
-  _transform(chunk, encoding, callback) {
-    const transformed = Number(chunk.toString()) * -1
-
-    console.log(transformed)
-
-    callback(null, Buffer.from(String(transformed)))
-  }
-}
-
 // req => ReadableStream
 // res => WritableStream
 
